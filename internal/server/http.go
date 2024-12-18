e9c7cfe2766738c5de1d84f0f1cbbaa86df9ff3a
Nathan Kimutai <nathanpykimutai@gmail.com>
2024-12-18 20:31:52 +0300

    wip: distributed log system

---
+++ internal/server/http.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type httpServer struct {
+	Log *Log
+}
+
+func newHTTPServer() *httpServer {
+	return &httpServer{
+		Log: NewLog(),
+	}
+}
+
+func NewHTTPServer(addr string) *http.Server {
+
+	r := mux.NewRouter()
+
+	return &http.Server{
+		Addr: addr,
+		Handler: r,
+	}
+}
+
+type ProduceRequest struct {
+	Record Record `json:"record"`
+}
+
+type ProduceResponse struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeRequest struct {
+	Offset uint64 `json:"offset"`
+}
+
+type ConsumeResponse struct {
+	Record Record `json:"record"`
+}
--- internal/server/log.go
+++ internal/server/log.go
@@ -25,6 +25,7 @@ func NewLog() *Log {
 func (c *Log) Append(record Record) (uint64,error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+
 	record.Offset = uint64(len(c.records))
 	c.records = append(c.records,record)
 	return record.Offset,nil
