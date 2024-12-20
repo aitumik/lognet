a1fc5b31761e2cb68dddd74a01c3093aea78ea35
Nathan Kimutai <nathanpykimutai@gmail.com>
2024-12-20 15:24:01 +0300

    implement writer and reader on store

--- internal/log/store.go
+++ internal/log/store.go
@@ -50,4 +50,54 @@ func (s *store) Append(p []byte) (n uint64,pos uint64,err error) {
 	if err := binary.Write(s.buf,enc,uint64(len(p))); err != nil {
 	       return 0,0,err
 	}
+
+	w,err := s.buf.Write(p)
+	if err != nil {
+	       return 0,0,err
+	}
+
+	w += lenWidth
+
+	s.size += uint64(w)
+	
+	return uint64(w),pos,nil
 }
+
+
+func (s *store) Read(pos uint64) ([]byte,err) {
+     	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+	       return nil,err
+	}
+
+	size := make([]byte,lenWidth)
+
+	// are we reading from s.File to size or what. What is
+	// the documentation for this crap
+
+	if _,err := s.File.ReadAt(size,int64(pos)); err != nil {
+	   	 return nil,err
+	}
+
+	b := make([]byte,lenWidth)
+	if _,err := s.File.ReadAt(b,enc.Uint64(s.pos + lenWidth)); err != nil {
+	   	 return nil,err
+	}
+
+	return b
+}
+
+func (s *store) ReadAt(p []byte,pos uint64) (int,error) {
+     	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.File.Flush(); err != nil {
+	   return 0,err
+	}
+
+	
+	return s.File.ReadAt(p,pos)
+}
+
