56562518c3d5f77a72bb124bbdb9fd8658272653
Nathan Kimutai <nathanpykimutai@gmail.com>
2025-01-20 18:09:04 +0300

    feat: getting back on distributed logger

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -1,10 +1,11 @@
 package log
 
 import (
-       "io/ioutil"
-       "os"
-       "testing"
-       "github.com/stretchr/testify/requre"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -13,6 +14,31 @@ var (
 )
 
 func TestStoreAppendRead(t *testing.T) {
-     	f,err := ioutil.TempFile("",
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	// Create a new store
+	store, err := newStore(f)
+
+	// Assert that there is no error
+	require.NoError(t, err)
+
+	// Test append
+	testAppend(t, store)
+
+	// Test Read
+	testRead(t, store)
+
+	// Test ReadAt
+	testReadAt(t, store)
 }
 
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+	}
+}
